9bba19007db7d68dcb086711b56c7f03c887a0a5
Harshvardhansinh Jadeja <harshvardhansinhjadeja49@gmail.com>
2024-02-02 11:39:57 +0530

    added workflow

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
 provider "aws" {
   region = "us-west-2"
-  profile = "harshvardhan-th"
+  # profile = "harshvardhan-th"
 }
 
+terraform {
+      backend "remote" {
+        organization = "harshvardhansinh_jadeja"
+        workspaces {
+          name = "harshvardhan-remote-cli"
+        }
+      }
+    }
 
 resource "aws_instance" "tf_ec2" {
     ami = "ami-0ecb0bb5d6b19457a"
